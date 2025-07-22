c7a218919d430ea377ce88607421253115fb5354
Saher <saher.ziauddin@dal.ca>
2025-07-22 18:10:37 -0300

    Created lambda using terraform

--- storage_and_retrieval.tf
+++ storage_and_retrieval.tf
@@ -1,10 +1,59 @@
-resource "aws_dynamodb_table" "orders" {
-  name = var.table_name
-  hash_key = "_id"
-  billing_mode = "PAY_PER_REQUEST"
+locals {
+  prefix = "terraform"
+}
 
-  attribute {
-    name = "_id"
-    type = "N"
+# resource "aws_dynamodb_table" "users" {
+#   name = var.table_name
+#   hash_key = "_id"
+#   billing_mode = "PAY_PER_REQUEST"
+
+#   attribute {
+#     name = "_id"
+#     type = "N"
+#   }
+# }
+
+# Lambda Creation
+# IAM role
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name               = "${local.prefix}_lambda_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+# Package the Lambda function code
+data "archive_file" "example" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/handler.py"
+  output_path = "${path.module}/lambda/function.zip"
+}
+
+# Lambda function
+resource "aws_lambda_function" "example" {
+  filename         = data.archive_file.example.output_path
+  function_name    = "lambda_handler"
+  role             = aws_iam_role.example.arn
+  handler          = "handler.lambda_handler"
+  source_code_hash = data.archive_file.example.output_base64sha256
+
+  runtime = "python3.13"
+
+  environment {
+    variables = {
+      ENVIRONMENT = "production"
+      LOG_LEVEL   = "info"
+    }
   }
 }
