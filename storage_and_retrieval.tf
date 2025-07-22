db71f69316690257c57a1f8bb9b6f538a1920d9b
Saher <saher.ziauddin@dal.ca>
2025-07-22 19:06:44 -0300

    Created Lambda function with access to CRUD operations in DynamoDB table

--- storage_and_retrieval.tf
+++ storage_and_retrieval.tf
@@ -2,35 +2,54 @@ locals {
   prefix = "terraform"
 }
 
-# resource "aws_dynamodb_table" "users" {
-#   name = var.table_name
-#   hash_key = "_id"
-#   billing_mode = "PAY_PER_REQUEST"
+resource "aws_dynamodb_table" "users" {
+  name = var.table_name
+  hash_key = "_id"
+  billing_mode = "PAY_PER_REQUEST"
 
-#   attribute {
-#     name = "_id"
-#     type = "N"
-#   }
-# }
-
-# Lambda Creation
-# IAM role
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
+  attribute {
+    name = "_id"
+    type = "N"
   }
 }
 
+# Lambda Creation
+# IAM role
 resource "aws_iam_role" "lambda_role" {
-  name               = "${local.prefix}_lambda_execution_role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  name = "${local.prefix}_lambda_dynamodb_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+data "aws_iam_policy_document" "lambda_dynamodb_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:Scan",
+      "dynamodb:Query"
+    ]
+    resources = [aws_dynamodb_table.users.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "lambda_dynamodb_policy_attachment" {
+  name   = "lambda_dynamodb_policy"
+  role   = aws_iam_role.lambda_role.name
+  policy = data.aws_iam_policy_document.lambda_dynamodb_policy.json
 }
 
 # Package the Lambda function code
