2d4c5dbdd50c67dcbf7acd454213dc9c9b21a354
Saher <saher.ziauddin@dal.ca>
2025-07-23 16:16:22 -0300

    Created API on API Gateway

--- lambda_read_only/handler.py
+++ lambda_read_only/handler.py
@@ -1,8 +1,15 @@
 import boto3
+import json
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('users')
 
 def lambda_handler(event, context):
   data = table.scan()
-  return data['Items']
+  return {
+    "statusCode": 200,
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "body": json.dumps(data['Items'])
+  }
--- storage_and_retrieval.tf
+++ storage_and_retrieval.tf
@@ -29,7 +29,7 @@ data "archive_file" "lambda_read_only" {
 
 resource "aws_lambda_function" "crud" {
   filename         = data.archive_file.lambda.output_path
-  function_name    = "DyanmoDbCrud"
+  function_name    = "Crud_DynamoDb"
   role             = aws_iam_role.lambda_role.arn
   handler          = "handler.lambda_handler"
   source_code_hash = data.archive_file.lambda.output_base64sha256
@@ -46,7 +46,7 @@ resource "aws_lambda_function" "crud" {
 
 resource "aws_lambda_function" "lambda_read_only" {
   filename = data.archive_file.lambda_read_only.output_path
-  function_name = "DynamoDbReadOnly"
+  function_name = "GetUsers_DynamoDb"
   role = aws_iam_role.lambda_read_only.arn
   handler = "handler.lambda_handler"
   source_code_hash = data.archive_file.lambda_read_only.output_base64sha256
@@ -59,4 +59,59 @@ resource "aws_lambda_function" "lambda_read_only" {
       LOG_LEVEL   = "info"
     }
   }
+}
+
+# API Gateway
+resource "aws_api_gateway_rest_api" "app" {
+  name = "delivery-app-api"
+  description = "REST Api for a delivery app"
+  endpoint_configuration {
+    types = [ "REGIONAL" ]
+  }
+}
+
+resource "aws_api_gateway_resource" "users" {
+  rest_api_id = aws_api_gateway_rest_api.app.id
+  parent_id = aws_api_gateway_rest_api.app.root_resource_id
+  path_part = "users"
+}
+
+# API Methods
+resource "aws_api_gateway_method" "get_users" {
+  rest_api_id = aws_api_gateway_rest_api.app.id
+  resource_id = aws_api_gateway_resource.users.id
+  http_method = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_users" {
+  rest_api_id = aws_api_gateway_rest_api.app.id
+  resource_id = aws_api_gateway_resource.users.id
+  http_method = aws_api_gateway_method.get_users.http_method
+
+  integration_http_method = "POST" # Always Post for Lambda
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.lambda_read_only.invoke_arn
+}
+
+resource "aws_lambda_permission" "app" {
+  statement_id = "AllowAPIGatewayInvoke"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_read_only.function_name
+  principal = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.app.execution_arn}/*/*"
+}
+
+# API Deployment Stage
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [ aws_api_gateway_integration.get_users ]
+
+  rest_api_id = aws_api_gateway_rest_api.app.id
+}
+
+resource "aws_api_gateway_stage" "deployment" {
+  stage_name = "dev"
+  rest_api_id = aws_api_gateway_rest_api.app.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  description = "Development stage"
 }
